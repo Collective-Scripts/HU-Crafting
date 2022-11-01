32a7b91257f5949904e8d47a6c08255d9dd4a43a
Collective <86518186+DevZiee@users.noreply.github.com>
2022-11-01 21:36:27 +0800

    feat(config): HU-Crafting v1

--- config.lua
+++ config.lua
@@ -3,21 +3,28 @@ Config = {}
 Config.NotificationType = { --['mythic_notify_default', 'mythic_notify_old', 'ox_lib', 't-notify', 'col_notify', 'col_notify_new', 'Roda_Notifications' 'esx_notify', 'default-esx']--
 	client = 'ox_lib',
     server = 'ox_lib'
-} 
+}
+
+Config.Webhook = "https://discord.com/api/webhooks/1007605587103711242/XMAJ-iJZho5I2BRzMETyS79lNWyFrtpRAs6x8sBo45Sbf0ZGvhE4vUIx0UTd8DF8G8Z4"
 
 Config.Crafting = {
     [1] = {
         CraftData = {
-            CraftLabel = '[~r~E~w~] Crafting',
-            craft_table = 'gr_prop_gr_bench_04a', -- [If you want to disable this feature, set this to false.]-
+            craft_table = 'gr_prop_gr_bench_04a', -- [If you want to disable this feature, set this to false.] --
             craft_table_coords = vec4(735.7, -1081.41, 22.17, 270.0),
+            TextUI = 'ox_lib', --[ps-ui, ox_lib, esx_textui]--
             CraftName = 'Weapon Crafting',
             CraftID = 'weapon_crafting', --[Please change this if you used ox_lib.]--
-            ContextMenu = 'esx_context', --[esx_context, ox_lib]--
+            ContextMenu = 'ox_lib', --[esx_context, ox_lib]--
             Craft = {
                 [1] = {
+                    blueprint_data = { -- [If you want to disable this feature, set this to false.] --
+                        enable = false,
+                        item = 'blueprint_carbinerifle',
+                        qyt = 1
+                    },
                     craft_time = 5000, -- [Change this if you use 'ox-circle' or 'ox-progress'.]
-                    craft_type = 'ox-progress', -- [ox-skillbar, ox-circle, ox-progress]--
+                    craft_type = 'ox-circle', -- [ox-skillbar, ox-circle, ox-progress]--
                     craft_type_data = {'easy', 'easy', {areaSize = 60, speedMultiplier = 2}, 'hard'}, -- [If you use 'ox-skillbar,' change this. / https://overextended.github.io/docs/ox_lib/Interface/Client/skillcheck]
                     craft_anim = {
                         dict = 'mini@repair',
@@ -40,6 +47,37 @@ Config.Crafting = {
                         {item = 'rocks', value = 5, label = 'Rocks'},
                         {item = 'iron', value = 1, label = 'Iron'},
                     },
+                },
+                [2] = {
+                    blueprint_data = { -- [If you want to disable this feature, set this to false.] --
+                        enable = false,
+                        item = 'blueprint_carbinerifle',
+                        qyt = 1
+                    },
+                    craft_time = 5000, -- [Change this if you use 'ox-circle' or 'ox-progress'.]
+                    craft_type = 'ox-skillbar', -- [ox-skillbar, ox-circle, ox-progress]--
+                    craft_type_data = {'easy', 'easy', {areaSize = 60, speedMultiplier = 2}, 'hard'}, -- [If you use 'ox-skillbar,' change this. / https://overextended.github.io/docs/ox_lib/Interface/Client/skillcheck]
+                    craft_anim = {
+                        dict = 'mini@repair',
+                        clip = 'fixing_a_ped',
+                        flag = 49,
+                        heading = 270.0
+                    }, 
+                    craft_object = {
+                        output_object = 'w_pi_pistol50', -- [https://forge.plebmasters.de/objects]--
+                        attach_object = `prop_tool_screwdvr02`,
+                        attach_pos = vec3(0.14, 0.0, -0.01),
+                        attach_rot = vec3(60.0, -147.0, 30.0),
+                        attach_bone = 57005
+                    },
+                    output_item = 'weapon_pistol50', 
+                    output_qyt = 1, 
+                    output_image = 'https://media.discordapp.net/attachments/1035636469626327110/1035636862972346479/WEAPON_PISTOL50.png',
+                    output_label = 'Pistol .50',
+                    input_items = {
+                        {item = 'rocks', value = 5, label = 'Rocks'},
+                        {item = 'iron', value = 1, label = 'Iron'},
+                    },
                 }
             }
         }
@@ -49,11 +87,17 @@ Config.Crafting = {
             CraftLabel = '[~r~E~w~] Crafting',
             craft_table = 'gr_prop_gr_bench_04a', -- [If you want to disable this feature, set this to false.]-
             craft_table_coords = vec4(735.34, -1088.9, 22.17, 268.14),
+            TextUI = 'ox_lib', --[ps-ui, ox_lib, esx_textui]--
             CraftName = 'Weapon Crafting',
             CraftID = 'weapon_crafting', --[Please change this if you used ox_lib.]--
-            ContextMenu = 'esx_context', --[esx_context, ox_lib]--
+            ContextMenu = 'ox_lib', --[esx_context, ox_lib]--
             Craft = {
                 [1] = {
+                    blueprint_data = { -- [If you want to disable this feature, set this to false.] --
+                        enable = false,
+                        item = 'blueprint_carbinerifle',
+                        qyt = 1
+                    },
                     craft_time = 5000, -- [Change this if you use 'ox-circle' or 'ox-progress'.]
                     craft_type = 'ox-progress', -- [ox-skillbar, ox-circle, ox-progress]--
                     craft_type_data = {'easy', 'easy', {areaSize = 60, speedMultiplier = 2}, 'hard'}, -- [If you use 'ox-skillbar,' change this. / https://overextended.github.io/docs/ox_lib/Interface/Client/skillcheck]
